a39c011f4ad208d2ee0fee1712e35781585bcc28
Eduardo Salomon <esalomon@student.ie.edu>
2020-10-13 13:47:19 +0200

    added exercise.py

---
+++ excercise.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 13 13:23:33 2020
+
+@author: edu
+"""
+#n = input("Input number: "
+def is_even(n) :
+    while n:
+        if n % 2 == 0 :
+            return True
+    else:
+       return False
+    
+    
